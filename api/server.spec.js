dba0ed723ddde3fb8e89416b6fd8cd65c0a2a911
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-18 22:44:01 -0700

    get and delete tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,7 @@
 const db = require("../data/dbConfig");
 const tarotdb = require("../tarot/tarotModel.js");
+const server = require("./server");
+const request = require("supertest");
 
 describe("tarot model", () => {
   describe("insert", () => {
@@ -19,6 +21,29 @@ describe("tarot model", () => {
       let tarot = await tarotdb.insert({ name: "Death", description: "death" });
       expect(tarot.name).toBe("Death");
       expect(tarot.description).toBe("death");
+    });
+  });
+  describe("get", () => {
+    it("should return 200 status", () => {
+      return request(server)
+        .get("/tarot")
+        .then(res => {
+          console.log(res);
+          expect(res.status).toBe(200);
+        });
+    });
+    it("should return JSON", async () => {
+      const res = await request(server).get("/tarot");
+      expect(res.type).toBe("application/json");
+    });
+  });
+  describe("delete", () => {
+    it("should return 200 status", () => {
+      return request(server)
+        .delete("/tarot/:id")
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
     });
   });
 });
